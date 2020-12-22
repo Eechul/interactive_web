b572841a6995afab678cf6a19b31d2c01cc82116
Dongd <choise154@gmail.com>
2020-12-22 17:55:41 +0900

    commit bounce ball Two

--- BouncingBall/js/ball.js
+++ BouncingBall/js/ball.js
@@ -1,14 +1,14 @@
 export class Ball {
 
-    constructor(canvasWidth, canvasHeight, redius) {
+    constructor(canvasWidth, canvasHeight, radius) {
         this.cWidth = canvasWidth
         this.cHeight = canvasHeight
         this.x = this.cWidth / 2
         this.y = this.cHeight / 1.5
-        this.redius = redius
-        this.diameter = this.redius * 2
-        this.fieldX = this.x
-        this.fieldY = this.y
+        this.radius = radius
+        this.diameter = this.radius * 2
+        this.fieldX = this.x + this.radius
+        this.fieldY = this.y + this.radius
         this.speed = 20.0
         this.directionX = this.speed
         this.directionY = this.speed
@@ -17,17 +17,17 @@ export class Ball {
 
     changeDirection(targetWidth, targetHeight) {
         // top
-        if((this.y + this.redius) < this.diameter) {
+        if((this.y + this.radius) < this.diameter) {
             this.directionY *= -1
         }
         // right
-        if(targetWidth < this.x + this.redius) {
+        if(targetWidth < this.x + this.radius) {
             if(this.x > this.fieldX) {
                 this.directionX *= -1
             }
         }
         // bottom
-        if(targetHeight < this.y + this.redius) {
+        if(targetHeight < this.y + this.radius) {
             if(this.x > this.fieldX) {
                 this.directionY *= -1
             } else if(this.x < this.fieldX) {
@@ -35,7 +35,7 @@ export class Ball {
             }
         }
         // left
-        if(this.x + this.redius < this.diameter) {
+        if(this.x + this.radius < this.diameter) {
             if(this.x < this.fieldX) {
                 this.directionX *= -1
             }
@@ -47,24 +47,31 @@ export class Ball {
         this.y += this.directionY;
 
         this.changeDirection(this.cWidth, this.cHeight)
+
+        const minX = obstacle.x - this.radius
+        const maxX = obstacle.maxX + this.radius
+        const minY = obstacle.y - this.radius
+        const maxY = obstacle.maxY + this.radius
+
         if(obstacle != null) {
-            if(
-                ((this.x + this.redius) >= obstacle.rangeX[0] && (this.x + this.redius) <= obstacle.rangeX[1])
-                &&
-                ((this.y + this.redius) >= obstacle.rangeY[0] && (this.y - this.redius) <= obstacle.rangeY[1])
-            ) {
-                console.log("2")
-                this.directionY *= -1
-                if(
-                    ((this.x + this.redius) >= obstacle.rangeX[0] && (this.x + this.redius) <= obstacle.rangeX[0])
-                        &&
-                    ((this.x + this.redius) <= obstacle.rangeY[0] && (this.x - this.redius) <= obstacle.rangeY[1])
-                ) {
-                    console.log("3")
-                    this.directionX *= -1
+            if(this.x > minX && this.x < maxX &&
+                this.y > minY && this.y < maxY) {
+
+                const x1 = Math.abs(minX - this.x)
+                const x2 = Math.abs(this.x - maxX)
+                const y1 = Math.abs(minY - this.y)
+                const y2 = Math.abs(this.y - maxY)
+
+                const min1 = Math.min(x1, x2)
+                const min2 = Math.min(y1, y2)
+                const min = Math.min(min1, min2)
+
+                if(min == min1) {
+                    this.directionX *=  -1
+                } if(min == min2) {
+                    this.directionY *=  -1
                 }
             }
-            // else
         }
 
         this.fieldX = this.x
@@ -75,7 +82,7 @@ export class Ball {
     draw(ctx, obstacle) {
         ctx.beginPath()
 
-        ctx.arc(this.x, this.y, this.redius,0, Math.PI * 2)
+        ctx.arc(this.x, this.y, this.radius,0, Math.PI * 2)
 
         ctx.fillStyle = "#c82124"
         ctx.fill()
--- BouncingBall/js/obstacle.js
+++ BouncingBall/js/obstacle.js
@@ -6,19 +6,19 @@ export class Obstacle {
         this.width = width;
         this.height = height;
         // index = 0 시작, index = 1 : 끝
-        this.rangeX = [this.x, this.x + this.width]
-        console.log(this.rangeX)
-        this.rangeY = [this.y, this.y + this.height]
-        console.log(this.rangeY)
+        this.minX = this.x
+        this.maxX = this.x + this.width
+        this.minY = this.y
+        this.maxY = this.y + this.height
     }
+
 
     draw(ctx) {
         ctx.beginPath()
 
         ctx.strokeRect(this.x, this.y, this.width, this.height)
         ctx.fill()
-        // ctx.strokeStyle = "#000000"
-        // ctx.stroke()
+
         ctx.closePath()
     }
 }
